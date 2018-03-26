e8ac15c0724adf2c76506706ab8289e0f880427b
VictorHugooc <victor.v.h.o.c@outlook.com>
2018-03-26 01:21:48 -0300

    Teste finalizado!

--- app/model/post.js
+++ app/model/post.js
@@ -3,27 +3,21 @@
 var schema = new mongoose.Schema({
   galeriaId: {
     type: Number,
-    required: true
   },
   youtubeId: {
     type: String,
-    required: true
   },
   titulo: {
     type: String,
-    required: true
   },
   subTitulo: {
     type: String,
-    required: true
   },
   conteudo: {
     type: String,
-    required: true
   },
   imagemThumbnailUrl: {
     type: String,
-    required: true
   }
 });
 
