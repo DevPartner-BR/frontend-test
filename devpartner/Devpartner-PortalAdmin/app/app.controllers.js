79030b0f695acbea5112e707fb4a0d9b335a7e37
Lauro-Soares <lauro_bs8@hotmail.com>
2019-01-13 22:16:25 -0200

    adjusts

--- devpartner/Devpartner-PortalAdmin/app/app.controllers.js
+++ devpartner/Devpartner-PortalAdmin/app/app.controllers.js
@@ -61,15 +61,6 @@ app.controller('loadMenu', ['$scope', '$http', '$rootScope', 'sysServicos', '$st
             }
         );
 
-        function meuController1($scope) {
-            $scope.title = 'algo';
-
-            $scope.todos = [
-                {text:'coisar' , when:'hoje'},
-                {text:'coisar3' , when:'amamnha'},
-                {text:'coisar9' , when:'depois'},
-            ]
-        }
         
         $scope.makeMenu = function (dataSet) {
             
